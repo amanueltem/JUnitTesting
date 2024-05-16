476280139d4d605757b4e5b748e4427b531444a1
Getahun Haftu <getahun19h@gmail.com>
2024-05-16 09:01:16 +0300

    test case for toBinary() method

--- ChangeNumberTest.java
+++ ChangeNumberTest.java
@@ -1,4 +1,9 @@
 
 public class ChangeNumberTest {
+    private ChangeNumber cNumber = new ChangeNumber();
 
+    @Test
+    public void testToBinary() {
+        assertEquals(10000, cNumber.toBinary(16)); // for birzaf
+    }
 }

3c45492dd0e532cd06bb3aefeb5b299b12b527cd
Gebremedhin Gebremeskel <gebremd2024@gmail.com>
2024-05-16 09:52:58 +0300

    toOctal() metheod implemented

--- ChangeNumber.java
+++ ChangeNumber.java
@@ -18,5 +18,16 @@ public class ChangeNumber {
         return result;
     }
 
-   
+    public int toOctal(int decimal) {
+        int counter = 0;
+        int remainder = 0;
+        int result = 0;
+        while (decimal != 0) {
+            remainder = decimal % 8;
+            result += (int) Math.pow(10, counter) * remainder;
+            decimal = decimal / 8;
+            counter++;
+        }
+        return result;
+    }
 }

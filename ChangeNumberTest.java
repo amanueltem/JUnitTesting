f04223f28efba920b9c903e74df05e1ef0d1c4fc
amanueltem <amanueltemesgen62@gmail.com>
2024-05-16 13:51:51 +0300

    i update the changeNumberTest class

--- ChangeNumberTest.java
+++ ChangeNumberTest.java
@@ -12,8 +12,7 @@ public class ChangeNumberTest {
     }
     @Test
     public void testToOctal() {
-        assertEquals(131, cNumber.toOctal(89));
-       // assertEquals(5,cNumber.toBinary(-27));
+        assertEquals(131, cNumber.toOctal(89));assertEquals(5,cNumber.toBinary(-27));
     }
     @Test
     public void testToHex() {

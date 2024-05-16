4113f60b440a9a417beadf6da67b0c57ab99cf14
Amanuel Temesgen <amanueltemesgen62@gmail.com>
2024-05-16 09:50:40 +0300

    I implemented toBinary() method

--- ChangeNumber.java
+++ ChangeNumber.java
@@ -1,4 +1,22 @@
 public class ChangeNumber {
 
+    public int toBinary(int decimal) {
+        int counter = 0;
+        int remainder = 0;
+        int result = 0;
+      if(decimal<0){
+
+      }
+        while (decimal != 0) {
+            remainder = decimal % 2;
+            result += remainder * (int) Math.pow(10, counter);
+            decimal = decimal / 2;
+            counter++;
+        }
+    
+
+        return result;
+    }
+
    
 }
